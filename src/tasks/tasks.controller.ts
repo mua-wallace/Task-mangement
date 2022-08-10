b6cea0e4b66a9f8be0ca610d84729600e4fbd4be
mua-wallace <muawallizewi@gmail.com>
2022-08-10 08:20:15 +0100

    first commit , crud for task

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,44 @@
+import { Task, TaskStatus } from './task.models';
+import { TasksService } from './tasks.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateTaskDTO } from './dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  getAlTasks(): Task[] {
+    return this.taskService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.taskService.getTaskById(id);
+  }
+  @Post()
+  createTask(@Body() createTaskDTO: CreateTaskDTO) {
+    return this.taskService.createTask(createTaskDTO);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.taskService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.taskService.updateTaskStatus(id, status);
+  }
+}
